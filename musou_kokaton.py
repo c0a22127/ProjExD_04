b264011e6e85f9a2fd92a5b53d32f1bfc99dd3fa
unknown <c0b2207762@edu.teu.ac.jp>
2023-10-24 16:31:19 +0900

    追加課題2:完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -249,6 +249,31 @@ class Score:
         screen.blit(self.image, self.rect)
 
 
+class NeoGravity(pg.sprite.Sprite):
+    """
+    重力場のクラス
+    発動時間：400フレーム
+    """
+    def __init__(self, life):
+        """
+        重力場Surfaceの生成
+        life:発動時間
+        """
+        super().__init__()
+        self.life = life
+        self.image = pg.Surface((WIDTH, HEIGHT), pg.SRCALPHA)
+        self.image.fill((0, 0, 0, 128))
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        """
+        発動時間を1減算し，0未満になったらkill
+        """
+        self.life -= 1
+        if self.life <= 0:
+            self.kill()
+
+
 def main():
     pg.display.set_caption("真！こうかとん無双")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -260,6 +285,7 @@ def main():
     beams = pg.sprite.Group()
     exps = pg.sprite.Group()
     emys = pg.sprite.Group()
+    neos = pg.sprite.Group()
 
     tmr = 0
     clock = pg.time.Clock()
@@ -270,6 +296,9 @@ def main():
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
+            if event.type == pg.KEYDOWN and event.key == pg.K_RETURN and score.score>= 20:
+                neos.add(NeoGravity(400))
+                score.score_up(-20) # スコア200を消費
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
@@ -289,6 +318,10 @@ def main():
             exps.add(Explosion(bomb, 50))  # 爆発エフェクト
             score.score_up(1)  # 1点アップ
 
+        for bomb in pg.sprite.groupcollide(bombs, neos, True, False).keys():
+            exps.add(Explosion(bomb, 50))  # 爆発エフェクト
+            score.score_up(1)  # 1点アップ
+            
         if len(pg.sprite.spritecollide(bird, bombs, True)) != 0:
             bird.change_img(8, screen) # こうかとん悲しみエフェクト
             score.update(screen)
@@ -305,6 +338,8 @@ def main():
         bombs.draw(screen)
         exps.update()
         exps.draw(screen)
+        neos.update()
+        neos.draw(screen)
         score.update(screen)
         pg.display.update()
         tmr += 1
